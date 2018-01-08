623745b58ce2ec697ca5ad6070cc4448dde0a9fc
Luis Quinones <alexx813@gmail.com>
2018-01-07 22:37:41 -0500

    refactore facebook strat

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,7 @@ module.exports = {
     googleClientSecret: 'dIpQ_S8QNKUzQKOwJ4hbtrHv',
     facebookAppId:'1823097487724333',
     facebookAppSecret : '04b852e34c862f0bc49d601853e03e5c',
-    mongoURI : 'mongodb://localhost:27017/digitalproject',
+mongoURI : 'mongodb://lex:password@ds135777.mlab.com:35777/digitalproject',
     cookieKey: 'ieufbuiebwuiwbefiewofiwefbweiufbifee',
     stripePublishableKey: 'pk_test_kebHchHkAA0o16jIWO7VTCYe',
     stripeSecretKey: 'sk_test_rhSjh2KemhZCSNNiVDdBBRGP',
@@ -12,6 +12,6 @@ module.exports = {
 };
 
 ///local db
-///mongodb://localhost:27017/
+///mongodb://localhost:27017/digitalproject
 //remote db
 ///mongodb://lex:password@ds135777.mlab.com:35777/digitalproject
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const FacebookStrategy = require('passport-facebook').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
@@ -17,6 +17,8 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+
+
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
@@ -27,56 +29,65 @@ passport.deserializeUser((id, done) => {
   });
 });
 
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
 //FACEBOOK STRATEGY
-// passport.use(new FacebookStrategy({
-//   clientID: keys.facebookAppId,
-//   clientSecret: keys.facebookAppSecret,
-//   callbackURL: "/auth/facebook/callback"
-// },
-// function (token, refreshToken, profile, done) {
+passport.use(new FacebookStrategy({
+  clientID: keys.facebookAppId,
+  clientSecret: keys.facebookAppSecret,
+callbackURL : "http://localhost:3000/login/facebook/return"
+},
+function (token, refreshToken, profile, cb) {
 
-// // asynchronous
-// process
-//   .nextTick(function () {
+// asynchronous
+process
+  .nextTick(function () {
 
-//     // find the user in the database based on their facebook id
-//     User
-//       .findOne({
-//         'facebook.id': profile.id
-//       }, function (err, user) {
+    // find the user in the database based on their facebook id
+    User
+      .findOne({
+        'facebook.id': profile.id
+      }, function (err, user) {
 
-//         // if there is an error, stop everything and return that ie an error connecting
-//         // to the database
-//         if (err) 
-//           return done(err);
+        // if there is an error, stop everything and return that ie an error connecting
+        // to the database
+        if (err) 
+          return cb(err);
         
-//         // if the user is found, then log them in
-//         if (user) {
-//           return done(null, user); // user found, return that user
-//         } else {
-//           // if there is no user found with that facebook id, create them
-//           var newUser = new User();
+        // if the user is found, then log them in
+        if (user) {
+          return done(null, user); // user found, return that user
+        } else {
+          // if there is no user found with that facebook id, create them
+          var newUser = new User();
 
-//           // set all of the facebook information in our user model
-//           newUser.facebook.id = profile.id; // set the users facebook id
-//           newUser.facebook.token = token; // we will save the token that facebook provides to the user
-//           newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
-//           newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+          // set all of the facebook information in our user model
+          newUser.facebook.id = profile.id; // set the users facebook id
+          newUser.facebook.token = token; // we will save the token that facebook provides to the user
+          newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+          newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
 
-//           // save our user to the database
-//           newUser.save(function (err) {
-//             if (err) 
-//               throw err;
+          // save our user to the database
+          newUser.save(function (err) {
+            if (err) 
+              throw err;
             
-//             // if successful, return the new user
-//             return done(null, newUser);
-//           });
-//         }
+            // if successful, return the new user
+            return done(null, newUser);
+          });
+        }
 
-//       });
-//   });
+      });
+  });
 
-// }));
+}));
 
 
 
