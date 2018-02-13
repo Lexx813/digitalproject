5fc2f6fb768e057a3ed325ecb27d72b535a25289
Luis Quinones <alexx813@gmail.com>
2018-02-13 12:49:34 -0500

    added register page

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -6,7 +6,12 @@ import Landing from '../components/Landing';
 import Dashboard from '../components/Dashboard';
 import SurveyNew from '../components/surveys/SurveyNew';
 import BlogNew from '../components/blogs/BlogNew';
+import Logout from '../components/admin/Logout';
+
+
 import LoginPage from '../containers/admin/Login';
+import Register from '../containers/admin/Register';
+
 
 // PAGES
 
@@ -38,6 +43,8 @@ class AppRouter extends Component {
             <Route exact path="/blog" component={Auth(BlogPage,null)} />
             <Route exact path="/contact" component={Auth(ContactPage,null)} />
             <Route exact path="/login" component={Auth(LoginPage)} />
+            <Route exact path="/logout" component={Auth(Logout,true)} />
+            <Route exact path="/register" component={Auth(Register,null)} />
             <Route path="/user" exact component={Auth(User, true)} />
             <Route path="/surveys/new" component={Auth(SurveyNew,true)} />
             <Route exact path="/dashboard" component={Auth(Dashboard,true)} />
